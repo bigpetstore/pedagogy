21780fdaab3e017e00e2b9764df7a6fd2e052a36
jayvyas <jay@apache.org>
2015-08-04 16:18:29 -0400

    first part of roullette sampler

--- ch1/roulette.py
+++ ch1/roulette.py
@@ -9,19 +9,26 @@ def roulette_wheel(model):
         (item, prob) = model[i]
         print(i,item,prob)
         cumProb += prob
-        wheel[i] = cumProb
+        wheel[i] = (item, cumProb)
     return wheel
 
 
+## Corresponds to the following roulette wheel...
+## [.1,.8, .9, .95, 1.0]
 wheel = roulette_wheel(
     [
-        ("dog food",0.7),
         ("fish food",0.1),
+        ("dog food",0.7),
         ("bird food",0.1),
         ("small rodent food",0.05),
         ("reptile food",0.05)
     ])
 
+### Now , we grab the bin that binds a random number.
+### For example
+### Random number = .1 ---> fish food.
+### Random number = .6 ---> dog food,
+### And so on...
 for i in range(1000):
-    r = math.floor(random.random()*len(wheel))
-    print(wheel[int(r)])
+    r = random.random()
+    print([it for (it, cp) in wheel if cp >= r ][0])
